c94dbcf618fe6914739d1bd15416c6f42294fe03
kele <damian.bogel@gmail.com>
2016-03-29 21:06:39 +0200

    Simple Process pool in Python

---
+++ iterate_dir.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from multiprocessing import Pool
+import subprocess
+
+import os
+import sys
+
+maindir = sys.argv[1]
+command = sys.argv[2]
+
+def run(filename):
+    os.system("{cmd} {fullpath} > {fullpath}_OUT".format(cmd=command,
+                                                         fullpath=maindir + "/" + filename))
+
+files = os.listdir(maindir)
+
+with Pool(processes=8) as pool:
+    print("Running {}".format(command))
+    pool.map(run, files)
+    pool.join()
+
